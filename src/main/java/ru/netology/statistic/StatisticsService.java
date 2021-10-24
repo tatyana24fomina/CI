e95e9f0d1677cf40bd561eac792918f85e1406b9
Tatyana Fomina <tatyana24fomina@gmail.com>
2021-10-24 21:57:16 +0400

    added report

--- src/main/java/ru/netology/statistic/StatisticsService.java
+++ src/main/java/ru/netology/statistic/StatisticsService.java
@@ -11,8 +11,16 @@ public class StatisticsService {
         long current_max_index = 0;
         long current_max = incomes[0];
         for (long income : incomes)
-            if (current_max < income)
+            if (current_max <= income)
                 current_max = income;
         return current_max;
+    }
+    public long findMin(long[] incomes) {
+        long current_min_index = 0;
+        long current_min = incomes[0];
+        for (long income : incomes)
+            if (current_min > income)
+                current_min = income;
+        return current_min;
     }
 }
